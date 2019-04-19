60b489ff19fe1b235c5d3199d7e16449597240c5
Admin <nehathakur.ab8@gmail.com>
2019-04-19 22:11:59 +0530

    Farming-Register OTP

---
+++ farmingproject/src/main/java/com/neha/farmingproject/model/CityList.java
@@ -0,0 +1,35 @@
+package com.neha.farmingproject.model;
+
+public class CityList {
+
+    public int id;
+    public  String name;
+    public String coords;
+
+    public CityList() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCoords() {
+        return coords;
+    }
+
+    public void setCoords(String coords) {
+        this.coords = coords;
+    }
+}
